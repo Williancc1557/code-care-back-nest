63ed9d484c5ed974b48830237b2826743b6a1dab
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2026-04-19 00:18:11 -0300

    refactor: remove unecessary imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './routes/auth/auth.module';
-import { DatabaseModule } from './providers/database/database.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     AuthModule,
-    DatabaseModule,
     ConfigModule.forRoot({ isGlobal: true }),
   ],
   controllers: [],
